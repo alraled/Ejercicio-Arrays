d3fcb5d5300638acd4aa7dae661b39731c152c12
alvaro <alraled@hotmail.es>
2016-01-22 14:14:42 +0100

    Arrays

---
+++ Ejercicio Arrays/JavaScript.js
@@ -0,0 +1,23 @@
+﻿var data = []
+function escribir() {
+    var capa = document.getElementById("capaAgenda");
+    capa.innerHTML = "";
+
+    for (var i = 0; i < data.lenght; i++) {
+        capa.innerHTML +=i+"-->"+ data[i] + "<br/>"
+    }
+
+}
+
+function add() {
+    var texto = document.getElementById("txtNombre");
+    data.push(texto.value);
+    texto.value = "";
+    escribir()
+}
+
+function borrar() {
+    var borrar = document.getElementById("txtBorrar");
+    data.splice(borrar.value, 1);
+    escribir();
+}
